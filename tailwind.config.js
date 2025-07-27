bdb93b25e4396d440b99e849dfcc5041250262ce
krd2ad <43148592+krd2ad@users.noreply.github.com>
2025-07-26 20:36:36 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./public/**/*.html"],
+  darkMode: 'class', // Required for toggle to work
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
